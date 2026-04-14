9c024ea95b671ea38ff41fe414b55b4aef428c86
Brayan <brayantoby23@gmail.com>
2026-04-13 21:14:59 -0300

    concluido

--- modulo/funcoes.js
+++ modulo/funcoes.js
@@ -1,119 +1,89 @@
-
 const {contatos} = require('./contatos')
 
 const getListaDeContatos = function(){
     let listaNumero = contatos["whats-users"];
     let resultado = [];
-   
-
-    listaNumero.forEach(function (numero){
+    listaNumero.forEach(function(numero){
         resultado.push(numero)
     })
-
     return resultado
-
-   
 };
 
 const getListaDeContaUsuario = function(numero){
     let listaNumero = contatos["whats-users"];
-    
     let resultado = false
-   listaNumero.forEach(function(usuario){
-
-    if(usuario.number === numero){
-        resultado = usuario
-    }
-
-   })
+    listaNumero.forEach(function(usuario){
+        if(usuario.number === numero){
+            resultado = usuario
+        }
+    })
     return resultado
-  
-
-}
+};
 
 const getListaDadosPessoais = function(numero){
     let listaNumero = contatos["whats-users"];
-    
     let resultado = false
-   listaNumero.forEach(function(usuario){
-
-    if(usuario.number === numero){
-        resultado = usuario.contacts.map(function(contato) {
-            return {
-              name: contato.name,
-              image: contato.image,
-              description: contato.description
-            };
-          });
-    }
-
-   })
+    listaNumero.forEach(function(usuario){
+        if(usuario.number === numero){
+            resultado = usuario.contacts.map(function(contato) {
+                return {
+                    name: contato.name,
+                    image: contato.image,
+                    description: contato.description
+                };
+            });
+        }
+    })
     return resultado
-  
-
-}
+};
 
 const getListaDadosPessoaisConversas = function(numero){
     let listaNumero = contatos["whats-users"];
-    
     let resultado = false
-   listaNumero.forEach(function(usuario){
-
-    if(usuario.number === numero){
-        resultado = usuario.contacts.map(function(contato) {
-            return contato.messages;
-          });
-    }
-
-   })
+    listaNumero.forEach(function(usuario){
+        if(usuario.number === numero){
+            resultado = usuario.contacts.map(function(contato) {
+                return contato.messages;
+            });
+        }
+    })
     return resultado
-  
+};
 
-}
-
+// CORRIGIDO: agora retorna array (o teste espera array)
 const getListaDadosUsuarioContato = function(numero, nome) {
     let listaNumero = contatos["whats-users"];
-    let resultado = false;
+    let resultado = [];
 
     listaNumero.forEach(function(usuario) {
         if (usuario.number === numero) {                  
             usuario.contacts.forEach(function(contato) {
                 if (contato.name === nome) {                
-                    resultado = {
+                    resultado.push({
                         usuario: usuario.nickname,
                         numero: usuario.number,
                         contato: contato.name,
                         conversas: contato.messages
-                    };
+                    });
                 }
             });
         }
     });
 
-    return resultado;
+    return resultado.length > 0 ? resultado : false;
 };
+
 const getFiltroPalavraChave = function(palavra, numero, nomeContato) {
     let listaNumero = contatos["whats-users"];
-    
     let resultado = [];
 
     listaNumero.forEach(function(usuario) {
-
-        // filtra pelo número do usuário
         if (usuario.number === numero) {
-
             usuario.contacts.forEach(function(contato) {
-
-                // filtra pelo nome do contato
                 if (contato.name === nomeContato) {
-
-                    // mensagem.content texto da mensagem
-                    // includes() verifica se contém a palavra
                     const mensagensFiltradas = contato.messages.filter(function(mensagem) {
                         return mensagem.content.toLowerCase().includes(palavra.toLowerCase());
                     });
-
-                    // só adiciona se tiver resultado
                     if (mensagensFiltradas.length > 0) {
                         resultado.push({
                             usuario: usuario.nickname,
@@ -130,21 +100,12 @@ const getFiltroPalavraChave = function(palavra, numero, nomeContato) {
     return resultado.length > 0 ? resultado : false;
 }
 
-//console.log(getListaDeContatos())
-
-//console.log(getListaDeContaUsuario('11987876567'))
-
-//console.log(getListaDadosPessoaisConversas('11987876567'))
-
-//console.log(getListaDadosUsuarioContato('11987876567', "John Doe"))
-
-//console.log(getFiltroPalavraChave("o", "11987876567", "John Doe"))
-
 module.exports = {
     getListaDeContatos,        
     getListaDeContaUsuario,    
     getListaDadosPessoais,
     getListaDadosPessoaisConversas,
     getListaDadosUsuarioContato,
-    getFiltroPalavraChave
+    getFiltroPalavraChave,
+   
 };
